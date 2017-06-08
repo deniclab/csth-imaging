f783ce38a22df1c120fc34160017ce44781970f1
nrweir <nicholas.r.weir@gmail.com>
2017-06-08 00:14:00 -0400

    final commit 20170607 inc

--- analysis_scripts/find_cells.py
+++ analysis_scripts/find_cells.py
@@ -4,9 +4,10 @@
 
 import czifile
 import numpy as np
+from warnings import warn
 from skimage import io
 import czi_io
-
+from scip import stats
 
 class CellMask:
     """Container for cell mask and intermediates."""
@@ -24,11 +25,49 @@ class CellFinder:
         if '.tif' in self.filename:
             self.cell_im = io.imread(self.filename)
         elif '.czi' in self.filename:
-            self.cell_im = czi_io.load_czi(self.filename)[0]
+            cell_czi = czi_io.load_czi(self.filename)
+            self.cell_im = cell_czi[0]
+            self.cell_channels = cell_czi[1]
         if '.tif' in self.bg_filename:
             self.bg_im = io.imread(self.bg_filename)
         elif '.czi' in self.bg_filename:
-            self.bg_im = czi_io.load_czi(self.bg_filename)
+            bg_czi = czi_io.load_czi(self.bg_filename)
+            self.bg_im = bg_czi[0]
+            self.bg_channels = bg_czi[0]
         # check inputs
-        if self.filename.shape != self.bg_filename.shape:
-            raise ValueError('bg image and cell images are different shapes.')
+        if self.filename.shape[0] != self.bg_filename.shape[0]:
+            warn('bg image and cell images have different #s of channels.')
+
+    def find_cells(self, channel):
+        """Find cells within image in the indicated channel."""
+
+        # get channel images first
+        im_arrs = self.get_channel_arrays(channel)
+        # transform into log space, as bg is roughly log-normal
+        log_f_im = np.log10(im_arrs[0])
+        log_bg_im = np.log10(im_arrs[1])
+        bg_mean = np.mean(log_bg_im)
+        bg_sd = np.std(log_bg_im)
+        # get p-val that px intensity could be brighter than the value in each
+        # array position in the "positive" im, which will indicate where
+        # fluorescence is.
+        f_pvals = 1-stats.norm.cdf(log_f_im, bg_mean, bg_sd)
+        # TODO NEXT: find sizes of contiguous regions, remove small ones
+
+
+
+    # helper methods #
+
+    def get_channel_arrays(self, channel, fluorescence=True, bg=True):
+        """A helper method for extracting im arrays for specific channels."""
+
+        channel = int(channel)  # Implicitly checks that channel is an int
+        return_vals = []
+        # return tuple of (fluorescence_array, bg_array) for the channel
+        if fluorescence:
+            return_vals.append(self.cell_im[np.argwhere(self.cell_channels ==
+                                                        channel), :, :, :])
+        if bg:
+            return_vals.append(self.bg_im[np.argwhere(self.bg_channels ==
+                                                      channel), :, :, :])
+        return tuple(return_vals)
