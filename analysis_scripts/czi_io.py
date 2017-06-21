b240c42b181ade940ab2ce6caebb4822e0bb8837
nrweir <nicholas.r.weir@gmail.com>
2017-06-21 10:21:29 -0400

    find_cells.py 0.0.1 complete

--- analysis_scripts/czi_io.py
+++ analysis_scripts/czi_io.py
@@ -12,7 +12,7 @@ import numpy as np
 import xml.etree.ElementTree as ET  # for parsing czi file metadata
 
 
-def load_czi(path):
+def load_single_czi(path):
     """Load .czi data into a numpy array and output along with wavelength IDs.
 
     Arguments:
@@ -22,7 +22,7 @@ def load_czi(path):
     Returns a tuple with 2 components:
     - a 4D numpy of shape [C,Z,Y,X]
     - a tuple with integers representing the excitation wavelengths for each
-      channels
+      channel
     """
     czi_file = czifile.CziFile(path)
     im_array = czi_file.asarray()  # extract image array from test_czi
@@ -43,3 +43,39 @@ def load_czi(path):
             im_array = np.expand_dims(im_array, axis=1)  # add Z axis
 
     return (im_array, channels)
+
+
+def load_multi_czi(path):
+    """Load multi-image czi file into a numpy array and output with wl IDs.
+
+    Arguments:
+        path (str): Path to the multi-image czi.
+
+    Returns a tuple consisting of two components:
+    - a 5D NumPy array with shape [IMG,C,Z,Y,X]
+    - a tuple with integers representing the excitation wavelengths for each
+      channel
+    """
+    czi_file = czifile.CziFile(path)
+    im_array = czi_file.asarray()  # extract image array from test_czi
+    if len(im_array.shape) == 8:  # if this is a single-image czi
+        return load_single_czi(path)
+    elif len(im_array.shape) == 9:
+        im_array = np.squeeze(im_array)  # eliminate single-dimensional axes
+        # get channels
+        channels = [np.rint(float(child.text)) for child in
+                    czi_file.metadata.iter('ExcitationWavelength')]
+        # if there was only one channel, this will get rid of the C dimension.
+        # add back if so.
+        if len(channels) == 1:
+            if len(im_array.shape) == 3:  # if it's not a Z stack but has >1 im
+                im_array = np.expand_dims(im_array, axis=1)  # once for channel
+                im_array = np.expand_dims(im_array, axis=1)  # once for Z
+            elif len(im_array.shape) == 4:  # if it is a Z-stack
+                im_array = np.expand_dims(im_array, axis=1)  # once for channel
+        else:
+            if len(im_array.shape) == 4:  # it has mult C but only 1 Z
+                im_array = np.expand_dims(im_array, axis=2)  # add Z axis
+        return (im_array, channels)
+    else:
+        raise ValueError('Received an unexpected czi file shape.')
