0107ba7ca3d8668ea426d4e554302f9a0b21fb58
nrweir <nicholas.r.weir@gmail.com>
2017-10-10 17:23:01 -0400

    adding get_tifs.py and .sh

---
+++ slurm_scripts/get_tifs.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import numpy as np
+import argparse
+from skimage import io
+sys.path.append('/n/denic_lab/Users/nweir/python_packages/')
+from csth_analysis import czi_io
+
+
+
+parser = argparse.ArgumentParser(description='Extract .tifs from czi files.')
+parser.add_argument('-d', '--czi_dir', required = True,
+                    help = 'directory containing images to segment.')
+parser.add_argument('-o', '--out_dir', required = True,
+                    help = 'directory to output tif files to.')
+
+args = parser.parse_args()
+print(args)
+czi_dir = args.czi_dir
+output_dir = args.out_dir
+images = [img[2:] for img in args.images]
+
+def main():
+    os.chdir(czi_dir)
+    if not output_dir.endswith('/'):
+        output_dir = output_dir + '/'
+    czi_list = [f for f in os.listdir() if '.czi' in f]
+    for f in czi_list:
+        czi_arr, channel_arr = czi_io.load_multi_czi
+        for im in range(0, czi_arr.shape[0]):
+            for c in range(0, czi_arr.shape[1]):
+                if channel_arr[c] == 561:
+                    channel = '594'
+                else:
+                    channel = str(channel_arr[c])
+                io.imsave(output_dir+f[:-3]+'_'+str(im)+'_'+channel+'.tif',
+                          czi_arr[im, c, :, :, :])
+
+
+if __name__ == '__main__':
+    main()
---
+++ slurm_scripts/get_tifs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH -t 0-3:00
+#SBATCH -p serial_requeue
+#SBATCH --mem=20000
+#SBATCH -o slurm.%N.%j.out # STDOUT
+#SBATCH -e slurm.%N.%j.err # STDERR
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nweir@fas.harvard.edu
+
+src_dir=$1
+dest_dir=$2
+
+source new-modules.sh
+source activate PYTO_SEG_ENV
+
+python3 /n/denic_lab/Users/nweir/python_packages/csth-imaging/slurm_scripts/get_tifs.py -d $src_dir -o $dest_dir
