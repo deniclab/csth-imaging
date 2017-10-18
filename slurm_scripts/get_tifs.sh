63a4a551d357b6d506e6c76625d158450df8ed39
Nicholas Weir <nweir@rclogin05.rc.fas.harvard.edu>
2017-10-18 17:27:29 -0400

    updating get_tifs.sh

--- slurm_scripts/get_tifs.sh
+++ slurm_scripts/get_tifs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH -n 1
-#SBATCH -t 0-3:00
+#SBATCH -t 0-6:00
 #SBATCH -p serial_requeue
-#SBATCH --mem=20000
+#SBATCH --mem=40000
 #SBATCH -o /n/denic_lab/Users/nweir/python_packages/csth-imaging/logs/%j.out # STDOUT
 #SBATCH -e /n/denic_lab/Users/nweir/python_packages/csth-imaging/logs/%j.err # STDERR
 #SBATCH --mail-type=ALL
